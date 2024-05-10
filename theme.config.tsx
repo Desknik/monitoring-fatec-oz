b70e86f7667bf1f4d19fb5141dca5cd93f67b6f5
Desknik <desknik@hotmail.com>
2024-05-10 12:56:07 -0300

    Atualização do blog

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,6 +2,17 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
+  head:(
+    <>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta property="og:title" content="Mentoria DSM - Fatec OZ" />
+      <meta property="og:description" content="Blog com conteúdos das aulas de monitoria ministradas no curso de Desenvolvimento de Software na Fatec Osasco" />
+      <link rel="apple-touch-icon" sizes="180x180" href="public\apple-touch-icon.png"/>
+      <link rel="icon" type="image/png" sizes="32x32" href="public\favicon-16x16.png"/>
+      <link rel="icon" type="image/png" sizes="16x16" href="public\favicon-16x16.png"/>
+      <link rel="manifest" href="/site.webmanifest"/>
+    </>
+  ),
   logo: <h2 style={{fontWeight: 'bold'}}>Mentory 📖</h2>,
   project: {
     link: 'https://github.com/Desknik/Monitoring',
@@ -12,7 +23,8 @@ const config: DocsThemeConfig = {
   docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
   footer: {
     text: '🧙‍♂️ Adagomir exists!',
-  },
+  }
+
 }
 
 
